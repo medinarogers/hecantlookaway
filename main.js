233272458b464c2a55dbc9c016b75563d807006e
medinarogers <129486657+medinarogers@users.noreply.github.com>
2023-11-02 15:46:46 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/eqBzVqWc69S0YFdM/scene.splinecode');
